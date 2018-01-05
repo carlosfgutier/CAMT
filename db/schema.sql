ae99b5dbe924cceea4431ea4a3b8840b472a2c2f
Twcapra <TateCapra@outlook.com>
2018-01-05 13:54:58 -0600

    Update to the SQL files. I added the syntax for the database to be created and the two different tables that will be needed to store the user id and the user workout information

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,40 @@
 -- Create the database to store the personal account information
--- DROP DATEBASE IF EXIST 
---  CREATE DATEBASE
+DROP DATEBASE IF EXIST acct_workouts_db;
+-- creats the database to store the personal accts of each user 
+CREATE DATEBASE acct_workouts_db;
+
+USE acct_workouts_db;
 	-- once the account informaiton is stored link the accounts and personal workouts
-	
+
+-- stores the personal acct information
+CREATE TABLE person_acct(
+	id_acct INTEGER NOT NULL AUTO_INCREMENT,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	acct_creation TIMESTAMP NOT NULL,
+
+	PRIMARY KEY (id_acct)
+);
+
+-- link the personal acct table with the workout table
+		-- this table will track the exercise that the user puts in 
+CREATE TABLE workouts(
+	id_workout INTEGER NOT NULL AUTO_INCREMENT,
+	id_acct INTEGER,
+	exercise_1 VARCHAR(255) NOT NULL,
+	exercise_2 VARCHAR(255),
+	exercise_3 VARCHAR(255),
+	exercise_4 VARCHAR(255),	
+	exercise_5 VARCHAR(255),
+	exercise_6 VARCHAR(255),
+	exercise_7 VARCHAR(255),
+	exercise_8 VARCHAR(255),
+	exercise_9 VARCHAR(255),
+	exercise_10 VARCHAR(255),
+
+	PRIMARY KEY (id_workout),
+	-- call from the tale above to get the foreign key
+	FOREIGN KEY (id_acct) REFERENCES person_acct(id_acct)
+); 
+
+SELECT 
