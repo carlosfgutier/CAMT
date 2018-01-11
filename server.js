9cb31a35835644ed183584f072521c767d9b4f33
Twcapra <TateCapra@outlook.com>
2018-01-11 13:00:22 -0600

    Update to any edit that I have made. I need to pull the current copy

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 // ########################################################################
 	// needs to call the route. I needf to create the routes file
 //require("./routes/api-routes.js")(app);
-
+require("./controllers/exercises_controllers.js");
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
